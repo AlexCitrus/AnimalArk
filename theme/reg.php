9b174df3ca82d0e0fa5f8faad86eaefd31c3a21a
tscunanan <tscunanan@mymail.mapua.edu.ph>
2023-01-29 19:17:17 +0800

    reg php upadte

--- theme/reg.php
+++ theme/reg.php
@@ -8,9 +8,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
     $lname = ($_POST['lname']);
 
     $bool = true;
-    $db_name = "usersdb";
-    $db_username = "root";
-    $db_pass = "";
+    $db_name = "id20217626_test";
+    $db_username = "id20217626_thea";
+    $db_pass = "Pass!1234567";
     $db_host = "localhost";
     $con = mysqli_connect("$db_host","$db_username","$db_pass", "$db_name") or die(mysqli_error()); //Connect to server
     $query = "SELECT * from users";
@@ -23,7 +23,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
         {
             $bool = false; // sets bool to false
             Print '<script>alert("Email has already been registered!");</script>'; //Prompts the user
-            Print '<script>window.location.assign("register.php");</script>'; // redirects to register.php
+            Print '<script>window.location.assign("index.html");</script>'; // redirects to register.php
         }
     }
 
