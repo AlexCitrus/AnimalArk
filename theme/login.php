fe6b8a6ebf686930fa53668ae7e75de2509bb18a
tscunanan <tscunanan@mymail.mapua.edu.ph>
2023-02-01 01:45:41 +0800

    iso chonkl

--- theme/login.php
+++ theme/login.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+if (isset($_POST['email']) && isset($_POST['pass'])) {
     $email = ($_POST['email']);
     $pass = ($_POST['pass']);
 
@@ -8,35 +9,44 @@ session_start();
     $db_pass = "Pass!1234567";
     $db_host = "localhost";
 
+    $con = mysqli_connect("$db_host", "$db_username", "$db_pass", "$db_name") or die(mysqli_error()); //Connect to server
 
-    $con = mysqli_connect("$db_host","$db_username","$db_pass", "$db_name") or die(mysqli_error()); //Connect to server
-    $query = "SELECT * from users WHERE email='$email' AND pass='$pass'";
-    $results = mysqli_query($con, $query);
-    
-    $table_users = "";
-    $table_password = "";
+    if (empty($email)) {
 
-    if($results) //IF there are no returning rows or no existing username
-    {
-        $row = mysqli_fetch_assoc($results); //display all rows from query
-        
-            $table_users = $row['email'];
-            $table_password = $row['pass'];
-        
-        if (($email == $table_users) && ($pass == $table_password)) // checks if there are any matching fields
-        {
-            $_SESSION['user'] = $email; //set the username in a session. This serves as a global variable
-            Print '<script>alert("Successfully Logged In!");</script>'; // Prompts the user
-            Print '<script>window.location.assign("cust_shop.php");</script>'; // redirects to login.php
-        }
-        else
-        {
-            Print '<script>alert("Incorrect Password!");</script>'; //Prompts the user
-            Print '<script>window.location.assign("login.html");</script>'; // redirects to login.php
+        header("Location: login.html?error=User Name is required");
+
+        exit();
+
+    }else if(empty($pass)){
+
+        header("Location: login.html?error=Password is required");
+
+        exit();
+
+    } else {
+        $query = "SELECT * from users WHERE email='$email' AND pass='$pass'";
+        $results = mysqli_query($con, $query);
+
+        if (mysqli_num_rows($results) === 1) {
+            $row = mysqli_fetch_assoc($results);
+
+            if ($row['email'] === $email && $row['pass'] === $pass) // checks if there are any matching fields
+            {
+                $_SESSION['email'] = $row['email'];
+
+                $_SESSION['fname'] = $row['fname'];
+
+                $_SESSION['id'] = $row['id'];
+                print '<script>alert("Successfully Logged In!");</script>'; // Prompts the user
+                print '<script>window.location.assign("cust_shop.php");</script>'; // redirects to login.php
+            } else {
+                print '<script>alert("Incorrect Password!");</script>'; //Prompts the user
+                print '<script>window.location.assign("login.html");</script>'; // redirects to login.php
+            }
+        } else {
+            print '<script>alert("This account does not exist!");</script>'; //Prompts the user
+            print '<script>window.location.assign("login.html");</script>'; // redirects to login.php
         }
     }
-    else {
-        Print '<script>alert("Error!");</script>'; //Prompts the user
-        Print '<script>window.location.assign("login.html");</script>'; // redirects to login.php
-    }
+}
 ?>
